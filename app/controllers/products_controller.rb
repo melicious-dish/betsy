6b39175b772538fb49a5a53e01ccde61b8afad04
Dionisia Montanez <dmontanez15@gmail.com>
2018-10-18 12:00:14 -0700

    added sessions, products, merchants crud actions. added omniauth.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,48 @@
 class ProductsController < ApplicationController
+  before_action :find_product, only [:show, :edit, :update]
+
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:status] = :success
+      flash[:result_text] = "Successfully created
+      #{@product}"
+      redirect_to product_path(@product)
+    else
+      flash[:status] = :failure
+      flash[:result_text] = "Could not create #{@product}"
+      flash[:messages] = @product.errors.messages
+      render :new, status: :bad_request
+    end
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :description, :photo_url, :status, :inventory)
+  end
+
+  def find_product
+    @product = Product.find_by(id: params[:id])
+    head :not_found unless @product
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,30 @@
 class SessionsController < ApplicationController
+
+  def create
+    auth_hash = request.env['omniauth.auth']
+    merchant = Merchant.find_by(uid: auth_hash[:uid], provider: 'github')
+
+    if merchant
+      flash[:success] = "Logged in as merchant #{merchant.username}"
+
+    else
+      merchant = Merchant.build_from_github(auth_hash)
+      if merchant.save
+        flash[:success] = "Logged in as new merchant #{merchant.username}"
+      else
+        flash[:error] = "Could not create new user account: #{merchant.errors.messages}"
+        redirect_to root_path
+        return
+      end
+      session[:username] = merchant.id
+      redirect_to root_path
+    end
+
+    def destroy
+    session[:username] = nil
+    flash[:status] = :success
+    flash[:success] = "Successfully logged out!"
+
+    redirect_to root_path
+  end
 end
