92e3835a14fcbd8a1609caac738dc4152efed9df
Melissa O'Hearn <melissa.ohearn@gmail.com>
2018-10-18 10:58:46 -0700

    add orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,57 @@
 class OrdersController < ApplicationController
-end
+
+before_action :find_order, only: [:show, :edit, :update, :destroy, :review]
+
+  def index
+  end
+
+  def new
+    @order = Order.new
+  end
+
+  def show
+    if @order.nil?
+      head :not_found
+    end
+  end
+
+  def create
+  end
+
+  def edit
+    if @order.nil?
+      head :not_found
+    end
+  end
+
+  def update
+    if @order.nil?
+      head :not_found
+  end
+
+  def destroy
+    if @order.nil?
+      head :not_found
+    end
+
+    #if @order.destroy
+      #redirect_to orders_path
+    #else
+      #render :show
+    #end
+  end
+
+  def review
+  end
+
+
+  private
+
+  def order_params
+    return
+    end
+
+    def find_order
+      @order = Order.find_by(id: params[:id])
+    end
+  end
