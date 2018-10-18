8202bd9d761af04744918a0103b4a495f1c8873c
Dionisia Montanez <dmontanez15@gmail.com>
2018-10-18 15:39:55 -0700

    add find_merchant method to application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :find_merchant
+
+  def render_404
+    # DPR: this will actually render a 404 page in production
+    raise ActionController::RoutingError.new('Not Found')
+  end
+
+private
+  def find_merchant
+    if session[:username]
+      @login_user = User.find_by(id: session[:username])
+    end
+  end
 end
