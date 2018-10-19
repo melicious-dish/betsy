911bc07e6267cbae47d2c42632fc95d16a7c81d8
Layla <layla.bristol@gmail.com>
2018-10-19 16:23:24 -0700

    uncommented some code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  # before_action :find_merchant
-  #
-  # def render_404
-  #   # DPR: this will actually render a 404 page in production
-  #   raise ActionController::RoutingError.new('Not Found')
-  # end
+  before_action :find_merchant
+
+  def render_404
+    # DPR: this will actually render a 404 page in production
+    raise ActionController::RoutingError.new('Not Found')
+  end
 
 private
   def find_merchant
