a059aaed1025d2a3a76b3ca2c423407bea1342d9
kangazoom <michellelcronin@gmail.com>
2018-10-20 00:06:08 -0700

    added validations and relations tests for merchant

--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -2,9 +2,63 @@ require "test_helper"
 
 describe Merchant do
   let(:merchant) { Merchant.new }
+  let(:mercury) { merchants(:mercury) }
 
-  it "must be valid" do
-    skip
-    value(merchant).must_be :valid?
+  describe 'relations' do
+    it 'has a list of products' do
+      expect(mercury).must_respond_to :products
+
+      mercury.products.each do |product|
+        expect(product).must_be_kind_of Product
+      end
+    end
+  end
+
+  describe 'validations' do
+    it "must be valid" do
+      value(mercury).must_be :valid?
+    end
+
+    it 'requires a username present' do
+      mercury.username = nil
+
+      expect(mercury.valid?).must_equal false
+      expect(mercury.errors.messages).must_include :username
+
+      mercury.username = "merch"
+      expect(mercury.valid?).must_equal true
+    end
+
+    it 'requires a unique username' do
+      mars = merchants(:mars)
+      saturn = merchants(:saturn)
+      saturn.username = "mars"
+
+      expect(saturn.valid?).must_equal false
+      expect(saturn.errors.messages).must_include :username
+    end
+
+    it 'requires an email address present' do
+      mercury.email = nil
+
+      expect(mercury.valid?).must_equal false
+      expect(mercury.errors.messages).must_include :email
+    end
+
+    it 'requires a unique email address' do
+      mars = merchants(:mars)
+      saturn = merchants(:saturn)
+      mars.email = "hello@hello.com"
+      saturn.email = "hello@hello.com"
+      mars.save
+      saturn.save
+
+      expect(saturn.valid?).must_equal false
+      expect(saturn.errors.messages).must_include :email
+    end
+  end
+
+  describe 'model logic' do
+    # TODO: add tests for business logic below
   end
 end
