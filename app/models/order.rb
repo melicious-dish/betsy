22c3b0ea67c9ee3d1041a65bca0f2166bda20dd2
Melissa O'Hearn <melissa.ohearn@gmail.com>
2018-10-20 13:29:38 -0700

    add actions to order and order_item

--- app/models/order.rb
+++ app/models/order.rb
@@ -10,10 +10,12 @@ class Order < ApplicationRecord
   # QUESTION: should not put order_item id down b/c it could then take many (ex: koolaid, nailpolish, etc.)
 
   def subtotal
-    order_items.collect { |oi| oi.valid? ? (oi.quantity * oi.unit_price) : 0 }.sum
+    order_items.collect { |oi| oi.valid? ? (oi.quantity * oi.price) : 0 }.sum
+
+    #products.collect { |p| p.valid? ? (p.price * )}
   end
   private
-  def set_order_status
+  def set_fullfillment_status
     self.order_status_id = 1
   end
 
@@ -22,3 +24,5 @@ class Order < ApplicationRecord
   end
 
 end
+
+# The set_order_status gets fired when the order is created and sets the fullfillment_status_id column to 1 (in progress). The update_subtotal function is called during save and sums up our order item's total cost and stores it in the subtotal field. The subtotal function actually overrides the field named subtotal, so calling order.subtotal calls the function. You can still access the field internally by calling self[:subtotal], so we use that to update the field. This allows us to dynamically update subtotal as needed. A side effect of this is user can't write any arbitrary value to the subtotal field.
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -4,6 +4,42 @@ class OrderItem < ApplicationRecord
 
   validates :quantity, presence: true, numericality: { greater_than: 0, only_integer: true }
 
+  validate :product_present
+  validate :order_present
+
+  before_save :finalize
   # QUESTION: add shipment? column: "Link to transition the order item to marked as shipped"
+
+# take the associated product's price if the order item is not currently persisted. If the order item is persisted then the contents of the unit_price field will be returned instead.
+
+  def unit_price
+    if persisted?
+      self[:unit_price]
+    else
+      product.price
+    end
+  end
+
+  def total_price
+    unit_price * quantity
+  end
+
+  private
+  def product_present
+    if product.nil?
+      errors.add(:product, "is not valid or is not active.")
+    end
+  end
+
+  def order_present
+    if order.nil?
+      errors.add(:order, "is not a valid order.")
+    end
+  end
+
+  def finalize
+    self[:unit_price] = unit_price
+    self[:total_price] = quantity * self[:unit_price]
+  end
 
 end
