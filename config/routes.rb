1d827905f06ec2b38a56c6d3731bb93ee3f6e455
Dionisia Montanez <dmontanez15@gmail.com>
2018-10-20 17:00:58 -0700

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,16 +5,18 @@ Rails.application.routes.draw do
   get "/auth/:provider/callback", to: "sessions#create"
   delete "/logout", to: "sessions#destroy", as: "logout"
 
+  resources :orders
+  resources :products
+  
   resources :categories do
     resources :products, include: [:index, :show, :create]
   end
-  
+
   resources :merchants do
     resources :products, include: [:index, :show, :create]
   end
 
-  resources :orders
-  resources :products
+
 
 
   # NOTE: nested route ideas: merchants/id#/products and categories/id#/products ???
