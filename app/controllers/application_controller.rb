dd28355656499af2d9dc4389610856cbbc66b48b
Melissa O'Hearn <melissa.ohearn@gmail.com>
2018-10-22 16:23:25 -0700

    save oder to cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,16 +13,22 @@ class ApplicationController < ActionController::Base
     if session[:order_id].nil? || session[:order_id].empty?
       new_order = Order.new()
       session[:order_id] = new_order.id
-      flash[:status] = :success
-      flash[:result_text] = "Added item to your cart"
-      redirect_to root_path
-      return new_order.id
-
+      result = new_order.save
+      if result
+        flash[:status] = :success
+        flash[:result_text] = "Added item to your cart"
+        redirect_to root_path
+        return new_order.id
+      else
+        flash[:status] = :failure
+        flash[:result_text] = "Could not add to order"
+        redirect_to root_path
+      end
 
     elsif
       ongoing_order = Order.find_by(id: session[:order_id])
       flash[:status] = :failure
-      flash[:result_text] = "Could not add to order"
+      flash[:result_text] = "Cart already open"
       redirect_to root_path
       return ongoing_order.id
     else
