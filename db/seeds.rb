38f33e3202c67f941457538bde809250876e214e
Dionisia Montanez <dmontanez15@gmail.com>
2018-10-22 10:53:38 -0700

    added categories show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Product.delete_all
-Product.create! id: 10, name: "Cherry", price: 22.49, status: true, merchant_id: 1
-Product.create! id: 11, name: "Apple", price: 0.29, status: true,  merchant_id: 1
-Product.create! id: 12, name: "Strawberries", price: 1.99, status: true,  merchant_id: 1
-
-Order.delete_all
-Order.create! id: 1, fulfillment_status: "In Progress"
-Order.create! id: 2, fulfillment_status: "Placed"
-Order.create! id: 3, fulfillment_status: "Shipped"
-Order.create! id: 4, fulfillment_status: "Cancelled"
-
-# Merchant.delete_all
-Merchant.create! id: 20, username: "Small Fry", email: "fry@gmail.com", uid: 33, provider: "github"
-Merchant.create! id: 21, username: "Momma", email: "momma@gmail.com", uid: 44, provider: "github"
-Merchant.create! id: 22, username: "Big Papa", email: "big_papa@gmail.com", uid: 55, provider: "github"
-Merchant.create! id: 23, username: "Disco", email: "disco@gmail.com", uid: 66, provider: "github"
+# Product.delete_all
+# Product.create! id: 10, name: "Cherry", price: 22.49, status: true, merchant_id: 1
+# Product.create! id: 11, name: "Apple", price: 0.29, status: true,  merchant_id: 1
+# Product.create! id: 12, name: "Strawberries", price: 1.99, status: true,  merchant_id: 1
+#
+# Order.delete_all
+# Order.create! id: 1, fulfillment_status: "In Progress"
+# Order.create! id: 2, fulfillment_status: "Placed"
+# Order.create! id: 3, fulfillment_status: "Shipped"
+# Order.create! id: 4, fulfillment_status: "Cancelled"
+#
+# # Merchant.delete_all
+# Merchant.create! id: 20, username: "Small Fry", email: "fry@gmail.com", uid: 33, provider: "github"
+# Merchant.create! id: 21, username: "Momma", email: "momma@gmail.com", uid: 44, provider: "github"
+# Merchant.create! id: 22, username: "Big Papa", email: "big_papa@gmail.com", uid: 55, provider: "github"
+# Merchant.create! id: 23, username: "Disco", email: "disco@gmail.com", uid: 66, provider: "github"
