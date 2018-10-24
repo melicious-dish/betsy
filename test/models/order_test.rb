e7f38fbc7b079871ed13337599720299f82688cf
Dionisia Montanez <dmontanez15@gmail.com>
2018-10-23 20:38:17 -0700

    tests for models/order_test with all passing.

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -3,6 +3,7 @@ require "test_helper"
 describe Order do
   let(:order) { Order.new }
   let(:order1) { orders(:order1) }
+  let(:order2) { orders(:order2) }
 
   describe 'relations' do
     it 'has many order items' do
@@ -21,7 +22,40 @@ describe Order do
   # TODO: decide/add more validations+testing (cc info, mailing address, etc)
   describe 'validations' do
     it "must be valid" do
-      value(order).must_be :valid?
+      expect(order2).must_be :valid?
     end
+
+    it 'must have an email address' do
+      expect(order2.guest_email).must_equal 'me@wnbc.net'
+      # test passes as long as it matches what's in the fixtures. Is this a good test?
+      expect(order2).must_respond_to :guest_email
+
+    end
+
+    it 'has a name corresponding with the credit card' do
+      expect(order2.guest_cc_name).must_equal 'Person Moo'
+    end
+
+    it 'has a credit card number with 16 digits' do
+      expect(order2.guest_cc_num).must_equal "0987654321098765"
+
+      expect(order2.guest_cc_num.length).must_equal 16
+    end
+
+    it 'has a three-digit CVV number' do
+      expect(order2.guest_cc_cvv_code).must_equal "321"
+
+      expect(order2.guest_cc_cvv_code.length).must_equal 3
+    end
+
+    it 'must have a 5 digit zip code' do
+      expect(order2.guest_cc_zip).must_equal "11238"
+
+      expect(order2.guest_cc_zip.length).must_equal 5
+    end
+
+    # it 'rejects an invalid order' do
+    #   invalid_order = Order.new
+    # end
   end
 end
